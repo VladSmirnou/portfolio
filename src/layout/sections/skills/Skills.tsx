14ff77b18489fc20e066e88514c4d523a76aa139
VladSmirnou <smr.vladislav@gmail.com>
2024-06-19 21:47:41 +0300

    refactor: adjust grid configuration

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -27,25 +27,18 @@ const GridWrapper = styled.div`
   display: grid;
   padding: 10px;
   grid-template-columns: repeat(auto-fill, 120px);
-  grid-auto-rows: 120px;
   gap: 74px 90px;
   justify-content: space-between;
 `;
 
 const SkillImg = styled.img`
-  width: 120px;
-  height: 120px;
+  width: 100%;
+  height: 100%;
   object-fit: none;
-`
+`;
 
 const StyledSkills = styled.section`
   margin-top: 144px;
-
-  ${FlexWrapper} {
-    row-gap: 74px;
-    column-gap: 90px;
-    padding: 10px;
-  }
 
   ${SectionHeading} {
     margin-bottom: 82px;
